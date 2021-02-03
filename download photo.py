2075953d92bb3931a90dca1c449475285fbd778c
caesar <76422688+Caesar723@users.noreply.github.com>
2021-02-03 11:52:47 +0800

    Update download photo.py

--- download photo.py
+++ download photo.py
@@ -1,28 +1,43 @@
 from urllib import request
 import json
-from bs4 import BeautifulSoup
+
 def get(n):
-    url="https://www.duitang.com/napi/blog/list/by_tag/?tag=%E5%92%92%E6%9C%AF%E5%9B%9E%E6%88%98&include_fields=top_comments,is_root,source_link,item,buyable,root_id,status,like_count,sender,album&limit="+str(n)+"&start=1&_=1612274425285"
-    header={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.56"}
-    req=request.Request(url,headers=header)
-    response=request.urlopen(req)
-    #print(type(response))
-    if response.getcode()==200:
-        data = response.read()
-        da= json.loads(data)
-        information=da["data"]["object_list"]
-        for i in range(0,n):
 
-            img=information[i]["photo"]["path"]
-            print(img)
-            img = request.Request(img, headers=header)
-            img = request.urlopen(img)
-            img = img.read()
+    yu = n % 98
+    zhen = int(n / 98)
+    if 0==0:
+        for ii in range(0,zhen+1):
+            start=ii*100
+            url = "https://www.duitang.com/napi/blog/list/by_tag/?tag=%E5%92%92%E6%9C%AF%E5%9B%9E%E6%88%98&include_fields=top_comments,is_root,source_link,item,buyable,root_id,status,like_count,sender,album&limit=" + str(
+                100) + "&start=" + str(start) + "&_=1612274425285"
+            header = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.56"}
+            req = request.Request(url, headers=header)
+            response = request.urlopen(req)
+            if response.getcode() == 200:
+                data = response.read()
+                da = json.loads(data)
+                information = da["data"]["object_list"]
+                if ii!=zhen :
+                    for i in range(0, 98):
+                        img = information[i]["photo"]["path"]
+
+                        img = request.Request(img, headers=header)
+                        img = request.urlopen(img)
+                        img = img.read()
+                        with open("photo/"+str(ii)+"-"+str(i)+".jpg",mode='wb') as f:
+                            f.write(img)
+                else:
+                    for i in range(0, yu):
+                        img = information[i]["photo"]["path"]
+                        print(img)
+                        img = request.Request(img, headers=header)
+                        img = request.urlopen(img)
+                        img = img.read()
+                        with open("photo/"+str(ii)+"-"+str(i)+".jpg",mode='wb') as f:
+                            f.write(img)
 
 
-            with open("photo/"+str(i)+".jpg",mode='wb') as f:
-                f.write(img)
-    #for i in range(0,1):
 
 
-get(30)
+get(100)
